b3ec65af831ab7e8ff9bd69ab7bccfb39cd7b382
Bart van Blokland (on MECHANINJA) <bart.van.blokland@idi.ntnu.no>
2016-03-29 14:05:22 +0200

    Fixed duplicate import

--- JournalImprover/src/data/Paper.java
+++ JournalImprover/src/data/Paper.java
@@ -13,5 +13,18 @@ public class Paper {
 		this.authors = authors;
 		this.abstractText = abstractText;
 	}
-
+	
+	@Override
+	public boolean equals(Object other) {
+		if(!(other instanceof Paper)) {
+			return false;
+		}
+		Paper otherPaper = (Paper) other;
+		return otherPaper.title.equals(title);
+	}
+	
+	@Override
+	public int hashCode() {
+		return title.hashCode();
+	}
 }
